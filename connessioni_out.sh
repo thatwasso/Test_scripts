e9055f4e5f9942eee74dce5eae750124017c3e20
thatwasso <46354493+thatwasso@users.noreply.github.com>
2022-08-03 16:29:00 +0000

    main script
    Testing and learning purposes
    Specifically made for LINUX DAY RIETI 2022

---
+++ connessioni_out.sh
@@ -0,0 +1,3 @@
+date >> $HOME/test/outconnections.log;
+netstat -an | grep ESTABLISHED | awk '{ print $5 }' | xargs -I {} echo "{}" >> $HOME/test/outconnections.log;
+echo " " >> $HOME/test/outconnections.log
